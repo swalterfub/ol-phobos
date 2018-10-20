275aa03b8d2fad949b836cfe5b7dc5136fd90866
Sebastian <s.walter@fu-berlin.de>
2018-10-20 09:38:53 +0200

    added cesium

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const path = require('path');
 
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopywebpackPlugin = require('copy-webpack-plugin');
+
 const cesiumSource = 'node_modules/cesium/Source';
 const cesiumWorkers = '../Build/Cesium/Workers';
 
@@ -36,6 +38,14 @@ module.exports = {
 	plugins: [
         	new HtmlWebpackPlugin({
 		template: 'src/index.html'
+		}),
+		// Copy Cesium Assets, Widgets, and Workers to a static directory
+	        new CopywebpackPlugin([ { from: path.join(cesiumSource, cesiumWorkers), to: 'Workers' } ]),
+	        new CopywebpackPlugin([ { from: path.join(cesiumSource, 'Assets'), to: 'Assets' } ]),
+	        new CopywebpackPlugin([ { from: path.join(cesiumSource, 'Widgets'), to: 'Widgets' } ]),
+		new webpack.DefinePlugin({
+			// Define relative base path in cesium for loading assets
+			CESIUM_BASE_URL: JSON.stringify('')
 		})
 	],
 	devServer: {
